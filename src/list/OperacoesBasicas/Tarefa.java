feb3feb0e165ece336f428cc4568b43912927f1e
CleidsonJs <Cleidsonjds@hotmail.com>
2023-10-09 14:43:13 -0300

    Desafio supervisionado ListaTarefas completo e funcional

--- src/list/OperacoesBasicas/Tarefa.java
+++ src/list/OperacoesBasicas/Tarefa.java
@@ -11,6 +11,10 @@ public class Tarefa {
     public Tarefa(String descricao) {
         this.descricao = descricao;
 
+    }
 
+    @Override
+    public String toString() {
+        return  descricao;
     }
 }
