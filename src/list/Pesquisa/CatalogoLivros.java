699acf6eb802b7b21dbb7979c199ce6f580fd948
CleidsonJs <Cleidsonjds@hotmail.com>
2023-10-10 00:39:29 -0300

    Desafio supervisionado de Catalogo de Livros

---
+++ src/list/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,73 @@
+package list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    //Atributo
+
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for(Livro l: livroList) {
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+        }
+        return livrosPorIntervaloAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo){
+        Livro livroPorTitulo = null;
+        if(!livroList.isEmpty()){
+            for(Livro l : livroList){
+                if(l.getTitulo().equalsIgnoreCase(titulo)){
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+
+    public static void main(String[] args) {
+        // Criando uma instância do catálogo de livros
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        // Adicionando livros ao catálogo
+        catalogoLivros.adicionarLivro("Microsserviços Prontos Para a Produção", "Susan J. Fowler", 2017);
+        catalogoLivros.adicionarLivro("Java Guia do Programador", "Peter Jandl Junior", 2021);
+        catalogoLivros.adicionarLivro("Código Limpo", "Robert C. Martin", 2009);
+        catalogoLivros.adicionarLivro("O Codificador Limpo", "Robert C. Martin", 2012);
+        catalogoLivros.adicionarLivro("O Codificador Limpo", "Robert C. Martin", 2011);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Robert C. Martin"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2019, 2021));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("O Codificador Limpo"));
+
+    }
+}
