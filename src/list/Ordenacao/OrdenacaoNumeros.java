bc805d905897e6c5481a61c392a73102c30fe628
CleidsonJs <Cleidsonjds@hotmail.com>
2023-10-10 01:51:28 -0300

    Desafio solo ordenação de Numeros com collections

---
+++ src/list/Ordenacao/OrdenacaoNumeros.java
@@ -0,0 +1,62 @@
+package list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumeros {
+    //atributos
+
+    private List<Integer> numeros;
+
+    public OrdenacaoNumeros() {
+        this.numeros = new ArrayList<>();
+    }
+    public void adicionarNumero(int numero){
+        numeros.add(numero);
+    }
+    public List<Integer> ordenarAscendente() {
+        List<Integer> numeroAscendente = new ArrayList<>(numeros);
+        Collections.sort(numeroAscendente);
+        return numeroAscendente;
+    }
+    public List<Integer> ordenarDescendente() {
+        List<Integer> numeroDescendente = new ArrayList<>(numeros);
+        numeroDescendente.sort(Collections.reverseOrder());
+        return numeroDescendente;
+    }
+    public void exibirNumeros() {
+        if (!numeros.isEmpty()) {
+            System.out.println(this.numeros);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Criando uma instância da classe OrdenacaoNumeros
+        OrdenacaoNumeros numeros = new OrdenacaoNumeros();
+
+        // Adicionando números à lista
+        numeros.adicionarNumero(2);
+        numeros.adicionarNumero(5);
+        numeros.adicionarNumero(4);
+        numeros.adicionarNumero(1);
+        numeros.adicionarNumero(99);
+
+        // Exibindo a lista de números adicionados
+        numeros.exibirNumeros();
+
+        // Ordenando e exibindo em ordem ascendente
+        System.out.println(numeros.ordenarAscendente());
+
+        // Exibindo a lista
+        numeros.exibirNumeros();
+
+        // Ordenando e exibindo em ordem descendente
+        System.out.println(numeros.ordenarDescendente());
+
+        // Exibindo a lista
+        numeros.exibirNumeros();
+    }
+}
